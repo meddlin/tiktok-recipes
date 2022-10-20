15779f68fa430076a77a1c233fc712162f1edbfd
meddlin <drushing.dev@gmail.com>
2022-10-20 02:15:23 -0500

    recipe page has split view

--- pages/recipes/[id].js
+++ pages/recipes/[id].js
@@ -42,27 +42,43 @@ export default function Recipe({ recipe }) {
             <p>
                 <i>{recipe.title}</i>
             </p>
-            <a href={recipe.url}>See the TikTok!</a>
-            <p>
-                <h3>Instructions</h3>
-                <ul>
-                    {(recipe && recipe.instructions && recipe.instructions.length) > 0 ? (
-                        (recipe.instructions.map((inst, idx) => {
-                            return (<li key={idx}>{inst}</li>);
-                        }))
-                    ) : ''}
-                </ul>
-            </p>
-            <p>
-                <h3>Ingredients</h3>
-                <ul>
-                    {(recipe && recipe.ingredients && recipe.ingredients.length) > 0 ? (
-                        (recipe.ingredients.map((ing, idx) => {
-                            return (<li key={idx}>{ing.item} {ing.amount}</li>);
-                        }))
-                    ) : ''}
-                </ul>
-            </p>
+            
+            <div className="recipe-container" style={{ display: `flex` }}>
+
+                {/* 
+                    TikTok video goes here 
+                */}
+                <div className="recipe-media" style={{ border: `1px solid black`, flexGrow: `1` }}>
+                    <a href={recipe.url}>See the TikTok!</a>
+                </div>
+
+                {/* 
+                    Actual recipe goes here 
+                */}
+                <div className="recipe" style={{ flexGrow: `1` }}>
+                    <div className="instructions">
+                        <h3>Instructions</h3>
+                        <ul>
+                            {(recipe && recipe.instructions && recipe.instructions.length) > 0 ? (
+                                (recipe.instructions.map((inst, idx) => {
+                                    return (<li key={idx}>{inst}</li>);
+                                }))
+                            ) : ''}
+                        </ul>
+                    </div>
+                    <div className="ingredients">
+                        <h3>Ingredients</h3>
+                        <ul>
+                            {(recipe && recipe.ingredients && recipe.ingredients.length) > 0 ? (
+                                (recipe.ingredients.map((ing, idx) => {
+                                    return (<li key={idx}>{ing.item} {ing.amount}</li>);
+                                }))
+                            ) : ''}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 };

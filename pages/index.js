e3313e2ce036b55a5a24aab2c2c78333cd8eb6dd
meddlin <drushing.dev@gmail.com>
2022-10-21 03:02:01 -0500

    Tailwind Installed! 😎
    - Default styles removed, some basic Tailwind stuff put in place

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { recipes } from './api/tiktoks'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="flex flex-col h-screen justify-between items-center">
+
+      {/* Main recipes list */}
       <main>
-        <h1> Recipes </h1>
+        <h1 className="text-2xl font-bold underline"> Recipes </h1>
         {recipes.map(rec => {
-          return <p key={rec.url}>
-            <b>{rec.title}</b> - <i><a href={rec.accountUrl}>{rec.account}</a></i>
-            <br />
-            <a href={rec.url}>{rec.url}</a>
-            <a href={`/recipes/${rec.id}`}>Recipe Page -- {rec.title}</a>
-          </p>
+          return (
+            <p key={rec.url} className="mt-5 mb-5">
+              <b>{rec.title}</b> - <i><a href={rec.accountUrl}>{rec.account}</a></i>
+              <br />
+              <a href={rec.url}>{rec.url}</a>
+              <br />
+              <a href={`/recipes/${rec.id}`}>Recipe Page -- {rec.title}</a>
+            </p>
+          );
         })}
       </main>
 
-      <footer className={styles.footer}>
-        Footer
+      {/* Footer */}
+      <footer className="">
+        <p className="text-xl">
+          Recipes collection curated from TikTok
+        </p>
       </footer>
     </div>
   )

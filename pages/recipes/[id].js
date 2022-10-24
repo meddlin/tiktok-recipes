9c25eab9321fdb8d61bbd62acb67bc44f66a7ef0
meddlin <drushing.dev@gmail.com>
2022-10-24 01:54:16 -0500

    Styling to dress things up
    - look at me, I can center a div

--- pages/recipes/[id].js
+++ pages/recipes/[id].js
@@ -34,11 +34,11 @@ export async function getStaticPaths() {
  */
 export default function Recipe({ recipe }) {
     return (
-        <div>
-            <h2>Recipe Page</h2>
-            <p>
-                <i>{recipe.title}</i>
-            </p>
+        <div className="flex flex-col items-center">
+            {/* <h2 className="text-xl font-bold">Recipe Page</h2> */}
+            <h2 className="text-xl font-bold italic">
+                {recipe.title}
+            </h2>
             
             <div className="recipe-container flex flex-row justify-center space-x-6">
 
@@ -52,21 +52,26 @@ export default function Recipe({ recipe }) {
                 */}
                 <div className="recipe mt-5 space-y-3">
                     <div className="ingredients">
-                        <h3 className="text-lg font-medium">Ingredients</h3>
-                        <ul>
+                        <h3 className="text-lg font-bold">Ingredients</h3>
+                        <ul className="ml-4">
                             {(recipe && recipe.ingredients && recipe.ingredients.length) > 0 ? (
                                 (recipe.ingredients.map((ing, idx) => {
-                                    return (<li key={idx}>{ing.item} {ing.amount}</li>);
+                                    return (<li key={idx} className="list-disc">{ing.amount} {ing.unit} {ing.item}</li>);
                                 }))
                             ) : ''}
                         </ul>
                     </div>
                     <div className="instructions">
-                        <h3 className="text-lg font-medium">Instructions</h3>
+                        <h3 className="text-lg font-bold">Instructions</h3>
                         <ul>
                             {(recipe && recipe.instructions && recipe.instructions.length) > 0 ? (
                                 (recipe.instructions.map((inst, idx) => {
-                                    return (<li key={idx}>{inst}</li>);
+                                    return (
+                                        <li key={idx}>
+                                            <h4 className="text-base font-semibold">Step {++idx}</h4>
+                                            {inst}
+                                        </li>
+                                    );
                                 }))
                             ) : ''}
                         </ul>

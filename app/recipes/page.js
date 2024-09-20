84c9e5d704c0eff858782f17952e89de5155628c
meddlin <drushing.dev@gmail.com>
2024-09-20 15:02:19 -0400

    Created recipes page - basic listing - search box at the top

---
+++ app/recipes/page.js
@@ -0,0 +1,62 @@
+export default function Recipes() {
+    const recipes = [
+        {
+            title: "Chocolate Chip Cookies",
+            description: "best homemade chocolate chip cookies you could make",
+            category: "dessert",
+            ingredients: [
+                {
+                    name: "egg",
+                    quantity: "2",
+                    unit: ""
+                },
+            ],
+            directions: [
+                {
+                    explanation: "Beat eggs, white sugar, brown sugar together"
+                },
+            ],
+            recipeCredit: {
+                url: "https://www.tiktok.com/t/ZTRHPMdSV/",
+                embedUrl: "https://www.tiktok.com/@tiggerninjafitness/video/7155596179513068846",
+                account: "tiggerninjafitness",
+                accountUrl: "https://www.tiktok.com/@tiggerninjafitness?_t=8WcwcNRWU5m&_r=1",
+            }
+        }
+    ]
+
+    return (
+        <>
+            <div className="flex flex-col justify-center items-center">
+                <div>
+                    <input placeholder="Search..." />
+                </div>
+                <div className="flex">
+                    {/* side nav */}
+                    <div>
+                        <ul>
+                            {'abcdefghijklmnopqrstuvwxyz'.split('').map(c => <li>{c}</li>)}
+                        </ul>
+                    </div>
+
+                    {/* recipe list */}
+                    <div>
+                        <ul>
+                            {recipes.length > 0 ? recipes.map((res, key) => {
+                                return (
+                                    <li key={key} className="mt-5 mb-5">
+                                        <b>{res.title}</b> - <i><a href={res.recipeCredit.accountUrl}>{res.recipeCredit.account}</a></i>
+                                        {/* <br />
+                                    <a href={res.url}>{res.url}</a> */}
+                                        <br />
+                                        <a href={`/sample/${res.id}`}>Recipe Page -- {res.title}</a>
+                                    </li>
+                                );
+                            }) : 'No recipes yet. Add one to get started!'}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}

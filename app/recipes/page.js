f146f899b3fbebd0f8fa1a73ce9f1bb7678c3113
meddlin <drushing.dev@gmail.com>
2024-09-21 00:25:54 -0500

    banana pudding

--- app/recipes/page.js
+++ app/recipes/page.js
@@ -18,6 +18,7 @@ export default function Recipes() {
                     explanation: "Beat eggs, white sugar, brown sugar together"
                 },
             ],
+            notes: "",
             recipeCredit: {
                 url: "https://www.tiktok.com/t/ZTRHPMdSV/",
                 embedUrl: "https://www.tiktok.com/@tiggerninjafitness/video/7155596179513068846",
@@ -46,6 +47,7 @@ export default function Recipes() {
                     explanation: "Mix in glass, pour over ice."
                 },
             ],
+            notes: "",
             recipeCredit: {
                 url: "",
                 embedUrl: "",
@@ -116,13 +118,72 @@ export default function Recipes() {
                     explanation: "Top with sauce and serve"
                 }
             ],
+            notes: "",
             recipeCredit: {
                 url: "https://www.tasteofhome.com/recipes/gnocchi-alfredo/",
                 embedUrl: "",
                 account: "",
                 accountUrl: "",
             }
-        }
+        },
+        {
+            title: "Mom's Banana Pudding",
+            description: "the one that gets requested",
+            category: "dessert",
+            ingredients: [
+                {
+                    name: "Bananas",
+                    quantity: "4",
+                    unit: ""
+                },
+                {
+                    name: "cool whip",
+                    quantity: "16",
+                    unit: "oz"
+                },
+                {
+                    name: "condensed milk",
+                    quantity: "8",
+                    unit: "oz"
+                },
+                {
+                    name: "instant vanilla pudding mix (small box)",
+                    quantity: "",
+                    unit: ""
+                },
+                {
+                    name: "mini nillas",
+                    quantity: "",
+                    unit: ""
+                },
+                {
+                    name: "lemon",
+                    quantity: "1",
+                    unit: ""
+                },
+            ],
+            directions: [
+                {
+                    explanation: "Slice bananas into 1/4 inch thick slices. Squeeze lemon juice acros banana slices.",
+                },
+                {
+                    explanation: "Make the pudding mix. Don't use milk. Cut water in half.",
+                },
+                {
+                    explanation: "Mix in cool whip and condensed milk",
+                },
+                {
+                    explanation: "Start with pudding in bottom of bowl. Layer pudding, banana slices, pudding, wafers...repeating.",
+                },
+            ],
+            notes: "Try for french vanilla pudding mix. Don't use milk. Cut water in half! Possibly put in freezer for easier assembly.",
+            recipeCredit: {
+                url: "",
+                embedUrl: "",
+                account: "",
+                accountUrl: "",
+            }
+        },
     ]
 
     function recipeCreditIsEmpty(creditObj) {

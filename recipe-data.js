9ce841426f27ccb5973e0b52223a66244f79b858
meddlin <drushing.dev@gmail.com>
2024-09-21 00:33:19 -0500

    moved recipe data

---
+++ recipe-data.js
@@ -0,0 +1,225 @@
+const recipes = [
+    {
+        title: "Chocolate Chip Cookies",
+        description: "best homemade chocolate chip cookies you could make",
+        category: "dessert",
+        ingredients: [
+            {
+                name: "egg",
+                quantity: "2",
+                unit: ""
+            },
+        ],
+        directions: [
+            {
+                explanation: "Beat eggs, white sugar, brown sugar together"
+            },
+        ],
+        notes: "",
+        recipeCredit: {
+            url: "https://www.tiktok.com/t/ZTRHPMdSV/",
+            embedUrl: "https://www.tiktok.com/@tiggerninjafitness/video/7155596179513068846",
+            account: "tiggerninjafitness",
+            accountUrl: "https://www.tiktok.com/@tiggerninjafitness?_t=8WcwcNRWU5m&_r=1",
+        }
+    },
+    {
+        title: "Dark Palaces",
+        description: "Dressed up 'old fashioned', inspired by James Joyce Irish pub in Baltimore",
+        category: "cocktails",
+        ingredients: [
+            {
+                name: "Four Roses bourbon",
+                quantity: "1",
+                unit: "oz"
+            },
+            {
+                name: "almond bitters",
+                quantity: "1-2",
+                unit: "dashes"
+            },
+        ],
+        directions: [
+            {
+                explanation: "Mix in glass, pour over ice."
+            },
+        ],
+        notes: "",
+        recipeCredit: {
+            url: "",
+            embedUrl: "",
+            account: "",
+            accountUrl: "",
+        }
+    },
+    {
+        title: "Gnocchi Alfredo",
+        description: "Cozy gnocchi in a creamy sauce",
+        category: "dinner",
+        ingredients: [
+            {
+                name: "potato gnocchi",
+                quantity: "1",
+                unit: "package"
+            },
+            {
+                name: "butter",
+                quantity: "",
+                unit: ""
+            },
+            {
+                name: "all-purpose flour",
+                quantity: "",
+                unit: ""
+            },
+            {
+                name: "milk",
+                quantity: "",
+                unit: ""
+            },
+            {
+                name: "parmesan cheese",
+                quantity: "",
+                unit: ""
+            },
+            {
+                name: "nutmeg",
+                quantity: "",
+                unit: ""
+            },
+            {
+                name: "portobello mushrooms",
+                quantity: "",
+                unit: ""
+            },
+            {
+                name: "parsley (optional)",
+                quantity: "",
+                unit: ""
+            }
+        ],
+        directions: [
+            {
+                explanation: "Make the pasta"
+            },
+            {
+                explanation: "Make sauce base"
+            },
+            {
+                explanation: "Stir in cheese"
+            },
+            {
+                explanation: "Cook the mushrooms, then combine"
+            },
+            {
+                explanation: "Top with sauce and serve"
+            }
+        ],
+        notes: "",
+        recipeCredit: {
+            url: "https://www.tasteofhome.com/recipes/gnocchi-alfredo/",
+            embedUrl: "",
+            account: "",
+            accountUrl: "",
+        }
+    },
+    {
+        title: "Mom's Banana Pudding",
+        description: "the one that gets requested",
+        category: "dessert",
+        ingredients: [
+            {
+                name: "Bananas",
+                quantity: "4",
+                unit: ""
+            },
+            {
+                name: "cool whip",
+                quantity: "16",
+                unit: "oz"
+            },
+            {
+                name: "condensed milk",
+                quantity: "8",
+                unit: "oz"
+            },
+            {
+                name: "instant vanilla pudding mix (small box)",
+                quantity: "",
+                unit: ""
+            },
+            {
+                name: "mini nillas",
+                quantity: "",
+                unit: ""
+            },
+            {
+                name: "lemon",
+                quantity: "1",
+                unit: ""
+            },
+        ],
+        directions: [
+            {
+                explanation: "Slice bananas into 1/4 inch thick slices. Squeeze lemon juice acros banana slices.",
+            },
+            {
+                explanation: "Make the pudding mix. Don't use milk. Cut water in half.",
+            },
+            {
+                explanation: "Mix in cool whip and condensed milk",
+            },
+            {
+                explanation: "Start with pudding in bottom of bowl. Layer pudding, banana slices, pudding, wafers...repeating.",
+            },
+        ],
+        notes: "Try for french vanilla pudding mix. Don't use milk. Cut water in half! Possibly put in freezer for easier assembly.",
+        recipeCredit: {
+            url: "",
+            embedUrl: "",
+            account: "",
+            accountUrl: "",
+        }
+    },
+    {
+        title: "Potato Salad",
+        description: "",
+        category: "",
+        ingredients: [
+            {
+                name: "Gold potatoes",
+                quantity: "",
+                unit: ""
+            },
+            {
+                name: "Eggs",
+                quantity: "3",
+                unit: ""
+            },
+            {
+                name: "mayo",
+                quantity: "",
+                unit: ""
+            },
+            {
+                name: "mustard",
+                quantity: "",
+                unit: ""
+            },
+        ],
+        directions: [
+            {
+                explanation: "",
+            }
+        ],
+        notes: "",
+        recipeCredit: {
+            url: "",
+            embedUrl: "",
+            account: "",
+            accountUrl: "",
+        }
+    },
+]
+
+export default recipes;

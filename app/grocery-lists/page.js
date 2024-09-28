f6a7ace2099651fdb2a831a38631ba83b3682fb8
meddlin <drushing.dev@gmail.com>
2024-09-28 14:49:47 -0500

    toggle between display, edit form
    - on grocery list

--- app/grocery-lists/page.js
+++ app/grocery-lists/page.js
@@ -1,5 +1,7 @@
 'use client';
 
+import { useState } from 'react';
+import DaisyButton from '@/components/DaisyButton';
 import { GroceryListModal, GroceryListModalContents, GroceryListModalDismissButton, GroceryListModalOpenButton } from '@/components/GroceryListModal';
 
 export default function GroceryLists() {
@@ -48,6 +50,8 @@ export default function GroceryLists() {
         }
     ]
 
+    const [showEditForm, setShowEditForm] = useState(false);
+
     return (
         <div className="">
             <h2>Grocery Lists</h2>
@@ -67,20 +71,35 @@ export default function GroceryLists() {
                                 </GroceryListModalOpenButton>
                                 <GroceryListModalContents>
                                     <div>
-                                        <div>{list.dateCreated}</div>
-                                        <div>Related recipes &gt;&gt;</div>
-                                        <ul>
-                                            {list.groceryItems && list.groceryItems.length > 0 ? list.groceryItems.map((item, index) => {
-                                                return (
-                                                    <li key={index}>
-                                                        <span>{item && item.quantity ? item.quantity : ''} {item && item.name ? item.name : ''}</span>
-                                                    </li>
-                                                )
-                                            }) : 'Add items to your grocery list'}
-                                        </ul>
+                                        <div className="flex flex-row justify-between">
+                                            <div className="font-medium text-slate-800">Grocery List: {list.dateCreated}</div>
+                                            <DaisyButton label="Edit" type="button" onClick={() => setShowEditForm(!showEditForm)} />
+                                        </div>
+                                        {showEditForm ? (
+                                            <form>
+                                                <p>Edit form goes here</p>
+                                            </form>
+                                        ) : (
+                                            <>
+                                                <ul>
+                                                    {list.groceryItems && list.groceryItems.length > 0 ? list.groceryItems.map((item, index) => {
+                                                        return (
+                                                            <li key={index}>
+                                                                <span>{item && item.quantity ? item.quantity : ''} {item && item.name ? item.name : ''}</span>
+                                                            </li>
+                                                        )
+                                                    }) : 'Add items to your grocery list'}
+                                                </ul>
+                                                <div>Related recipes &gt;&gt;</div>
+                                            </>
+                                        )}
                                     </div>
                                     <GroceryListModalDismissButton>
-                                        <div>CLOSE</div>
+                                        <DaisyButton 
+                                            label="Dismiss" 
+                                            type="button" 
+                                            className="mt-12"
+                                        />
                                     </GroceryListModalDismissButton>
                                 </GroceryListModalContents>
                             </GroceryListModal>
